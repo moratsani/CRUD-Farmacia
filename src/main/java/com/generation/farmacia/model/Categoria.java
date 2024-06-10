f5d2ab13734ecba14c599b8da07c20abcd8b2546
Eliana Morato <liamorato@hotmail.com>
2024-06-10 10:40:29 -0300

    CRUD de categoria Criação dos packages model, repository e controller Criação da Model de Categoria Criação da Repository Categoria Criação da Controller Categoria

---
+++ src/main/java/com/generation/farmacia/model/Categoria.java
@@ -0,0 +1,50 @@
+package com.generation.farmacia.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_categoria")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O atributo nome não pode estar em branco")
+	@Size(min = 2, max = 100, message = "O atributo nome aceita o mínimo de 2 caracteres e máximo de 100 caracteres")
+	private String nome;
+	
+	@Size (max = 500, message = "O atributo descrição só aceita até 500 caracteres")
+	private String descricao;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+}
